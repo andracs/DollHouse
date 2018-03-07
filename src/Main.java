3630072aaa8b60a7c23efdbe7d8afbc997053cc5
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-03-07 21:21:56 +0100

    Har rettet fejlen, så nu virker vores singleton.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
         DollHouseSingleton d1 = DollHouseSingleton.getInstance();
         DollHouseSingleton d2 = DollHouseSingleton.getInstance();
 
-        if (d1.equals(d2)) {
+        if (d1 == d2) {
             System.out.println("De to dollhouses er det samme sgu!");
         }
         else
